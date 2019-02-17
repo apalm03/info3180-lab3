99a97fa5ed02c7426447e1bd8e272b57ee547632
Aaron Myrie <aaron477myrie@gmail.com>
2019-02-17 23:35:37 +0000

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'iloveformalmethods'
 app.config['MAIL_SERVER']= 'smtp.mailtrap.io'
 app.config['MAIL_PORT']=  '2525'
-app.config['MAIL_USERNAME'] = '2121ef427b7b0e'
-app.config['MAIL_PASSWORD'] = '34628590fdc253'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 
 mail = Mail(app)
 from app import views
